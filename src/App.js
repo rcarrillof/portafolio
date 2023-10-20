03c0b0a311f574e67262af36886074cc0fbe3f5d
rcarrillof <ricafo@hotmail.com>
2023-10-19 22:21:34 -0600

    Segundo commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+/* este es con el que funciona tiene el "TRANSITION GROUP", CONFIGURAR LOS ESTILOS CON ESTA NUEVA CONFI */
+ import React from 'react';
 import { Routes, Route, useLocation } from 'react-router-dom';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import './App.css';
@@ -18,10 +19,9 @@ function App() {
         <CSSTransition
           key={location.key}
           classNames="fade"
-          timeout={300}
-        >
+          timeout={300}>
           <Routes>
-            <Route path="/Home" element={<Home />} />
+            <Route path="/Home" element={<Home />} index /> 
             <Route path="/Proyectos" element={<Proyectos />} />
             <Route path="/Contacto" element={<Contacto />} />
           </Routes>
@@ -32,3 +32,25 @@ function App() {
 }
 
 export default App;
+
+/* import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './componentes/Home';
+import Proyectos from './componentes/Proyectos';
+import Contacto from './componentes/Contacto';
+import './App.css';
+
+function App() {
+  return (
+    <div className="Container_principal">
+      <Routes>
+        <Route path="/Home" element={<Home />} index />
+        <Route path="/Proyectos" element={<Proyectos />} />
+        <Route path="/Contacto" element={<Contacto />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
+ */
--- src/componentes/Contacto.js
+++ src/componentes/Contacto.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './Contacto.css';
+import fb from './img/fb_2.png';
+import ig from './img/ig.png';
+import linkedin from './img/linkedin.png';
 
 const Contacto = () => {
   return (
@@ -7,6 +10,7 @@ const Contacto = () => {
 
       <div className="card">
         <div className="card-content">
+          <img src={fb} alt="logo facebook" className='Logo_fb' />
           <a href="https://www.facebook.com/ricardo.carrillofonseca" target="_blank" rel="noopener noreferrer" className="link">
             <h2>Facebook</h2>
             <p>Ricardo Carrillo Fonseca</p>
@@ -15,7 +19,9 @@ const Contacto = () => {
       </div>
 
       <div className="card">
+
         <div className="card-content">
+          <img src={ig} alt="logo Instagram" className='Logo_ig' />
           <a href='https://www.instagram.com/r_carrillo11/' target="_blank" rel="noopener noreferrer" className="link">
             <h2>Instagram</h2>
             <p>r_carrillo11</p>
@@ -25,9 +31,10 @@ const Contacto = () => {
 
       <div className="card">
         <div className="card-content">
-        <a href='https://www.linkedin.com/in/ricardo-carrillo-fonseca-081a641a2/' target="_blank" rel="noopener noreferrer" className="link">
-          <h2>linkedin</h2>
-          <p>Ricardo Carrillo Fonseca</p>
+        <img src={linkedin} alt="logo linkedin" className='Logo_linkedin' />
+          <a href='https://www.linkedin.com/in/ricardo-carrillo-fonseca-081a641a2/' target="_blank" rel="noopener noreferrer" className="link">
+            <h2>linkedin</h2>
+            <p>Ricardo Carrillo Fonseca</p>
           </a>
         </div>
       </div>
